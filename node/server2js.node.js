cda392832f8a49607eb679dffd1694c7f1bcd697
Thanasis Polychronakis <thanpolas@gmail.com>
2013-02-22 02:31:56 +0200

    node now escapes single quotes too v2

--- node/server2js.node.js
+++ node/server2js.node.js
@@ -42,7 +42,7 @@ exports.add = function(req, operation, data) {
   var jsonData = JSON.stringify(data);
   var encData = gString.htmlEscape(jsonData);
   // escape single quotes
-  var escaped = encData.replace("'", "\\'");
+  var escaped = encData.replace("'", "\'");
   req[storeSpace].queue.push({op: operation, val: escaped});
   req[storeSpace].hash[operation] = data;
   // reset encString
